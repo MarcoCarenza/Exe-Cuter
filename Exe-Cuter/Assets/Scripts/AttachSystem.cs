6f593c22b51f8ebd88f3e87351355b1733588014
Hansong Li <hl6349@nyu.edu>
2025-11-13 10:30:52 -0500

    Attachment sample
    Added Sample Scene for Attaching, and scripts

---
+++ Exe-Cuter/Assets/Scripts/AttachSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AttachSystem : MonoBehaviour
+{
+    public static Attachable selectedAttachable;
+    public void SelectNewAttachable(Attachable newAttachable)
+    {
+        // Deselect the previous one
+        if (selectedAttachable != null && selectedAttachable != newAttachable)
+            ClearSelectedAttachable();
+
+        // Select the new one
+        selectedAttachable = newAttachable;
+        selectedAttachable.SelectEffect();
+    }
+
+    public static void ClearSelectedAttachable()
+    {
+        selectedAttachable.DeselectEffect();
+        selectedAttachable = null;
+    }
+}
